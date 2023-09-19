12e3c5c5cf7d0f53860b5a43daa574682bfdabc1
Tavot <guerreroagustavo@hotmail.com>
2023-09-18 22:04:58 -0300

    Cambios

--- src/public/js/payment.js
+++ src/public/js/payment.js
@@ -4,32 +4,39 @@ let payContainer = document.querySelector('#payForm')
 paymentBtn.addEventListener('click', async (event)=>{
     event.preventDefault()
 
-    try {
-        fetch('/api/payment/payment-intents', {
-            method: 'POST',
-            headers: {
-                'Content-Type':'application/json',
-            }
-        })
-        .then(resp => resp.json())
-        .then(resp => {
-            console.log(resp)
-            console.log(resp.status.error==='error')
+
+    //Proximamente.
+    
+    // try {
+    //     fetch('/api/payment/payment-intents', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type':'application/json',
+    //         }
+    //     }
+    //     )
+    //     .then(resp => resp.json())
+    //     .then(resp => {
+    //         console.log(resp)
+    //         console.log(resp.status.error==='error')
             
-            if(!(resp.status==='error')){
-                payContainer.innerHTML = `  <form action="">
-                                                <p>Ingrese el numero de la tarjeta</p>
-                                                <input type="number">
+    //         if(!(resp.status==='error')){
+    //             payContainer.innerHTML = `  <form action="">
+    //                                             <p>Ingrese el numero de la tarjeta</p>
+    //                                             <input type="number">
                                         
-                                                <p>Ingrese la fecha de expiracion de la tarjeta</p>
-                                                <input type="number">
+    //                                             <p>Ingrese la fecha de expiracion de la tarjeta</p>
+    //                                             <input type="number">
                                         
-                                                <p>Ingrese el CVC de la tarjeta</p>
-                                                <input type="number">
-                                            </form>`
-            }
-        })
-    } catch (error) {
-        console.log(error);   
-    }
+    //                                             <p>Ingrese el CVC de la tarjeta</p>
+    //                                             <input type="number">
+    //                                         </form>`
+    //         }
+    //     })
+    // } catch (error) {
+    //     console.log(error);   
+    // }
+
+
+    
 })
--- src/ultis/logger.js
+++ src/ultis/logger.js
@@ -25,6 +25,7 @@ const logger = winston.createLogger({
         new winston.transports.Console({
             level: 'debug',
             format: winston.format.combine(
+                //Esta Colorizacion Rompe Codigo.
                 // winston.format.colorize({colors: customLevels.colors}),
                 winston.format.simple()
             )
