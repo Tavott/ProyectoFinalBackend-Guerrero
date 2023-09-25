6a3471a2f02df4a7946428494bd3568386933122
Tavot <guerreroagustavo@hotmail.com>
2023-09-25 19:07:30 -0300

    v2

--- src/public/js/payment.js
+++ src/public/js/payment.js
@@ -1,65 +1,35 @@
-let paymentBtn = document.querySelector('#pago');
+let paymentBtn = document.querySelector('#pago')
+let payContainer = document.querySelector('#payForm')
 
-paymentBtn.addEventListener('click', async (event) => {
-    event.preventDefault();
+paymentBtn.addEventListener('click', async (event)=>{
+    event.preventDefault()
 
     try {
-
-        const amountValue = document.querySelector('#payForm').value; // Asume que tienes un input con id "amountInput"
-
-        // Construye el objeto de datos para la solicitud POST
-        const postData = {
-            amount: amountValue, // Asigna el valor del amount
-            // Otros campos de datos si es necesario...
-        };
-        
-        // Realiza una solicitud POST a la API de payment-intents
-        const response = await fetch('/api/payment/payment-intents', {
+        fetch('/api/payment/payment-intents', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(postData), // Convierte el objeto de datos a JSON
-        });
-
-        // Comprueba si la respuesta es exitosa
-        if (response.ok) {
-            const data = await response.json();
-
-            // Comprueba si el estado es "error" en lugar de "status.error"
-            if (data.status === 'error') {
-                console.error('Error en la respuesta:', data.error);
-            } else {
-                // Abre el resultado en una nueva ventana o pestaña
-                const newWindow = window.open('', '_blank');
-                if (newWindow) {
-                    // Escribe el contenido de payContainer en la nueva ventana
-                    newWindow.document.write(`
-                        <html>
-                        <head>
-                            <title>Página de Pago</title>
-                        </head>
-                        <body>
-                            <h1>Formulario de Pago</h1>
-                            <form action="">
-                                <p>Ingrese el número de la tarjeta</p>
-                                <input type="number">
-                                <p>Ingrese la fecha de expiración de la tarjeta</p>
-                                <input type="number">
-                                <p>Ingrese el CVC de la tarjeta</p>
-                                <input type="number">
-                            </form>
-                        </body>
-                        </html>
-                    `);
-                } else {
-                    console.error('No se pudo abrir una nueva ventana.');
-                }
+                'Content-Type':'application/json',
             }
-        } else {
-            console.error('Error en la solicitud:', response.status);
-        }
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            console.log(resp)
+            console.log(resp.status.error==='error')
+            
+            if(!(resp.status==='error')){
+                payContainer.innerHTML = `  <form action="">
+                                                <p>Ingrese el numero de la tarjeta</p>
+                                                <input type="number">
+                                        
+                                                <p>Ingrese la fecha de expiracion de la tarjeta</p>
+                                                <input type="number">
+                                        
+                                                <p>Ingrese el CVC de la tarjeta</p>
+                                                <input type="number">
+                                            </form>`
+            }
+        })
     } catch (error) {
-        console.error('Error inesperado:', error);
+        console.log(error);   
     }
-});
+})
