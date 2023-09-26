c6666e7fd03c5aa191aba293dd413830a8f3876d
Tavot <guerreroagustavo@hotmail.com>
2023-09-25 23:27:21 -0300

    v2

--- src/routes/views.js
+++ src/routes/views.js
@@ -1,14 +1,14 @@
 import { Router } from "express"
 import { auth } from "../middleware/auth.js"
 import ViewsController from "../controllers/viewsController.js"
-import LoginController from "../controllers/loginController.js"
+
 import { rollAdminVerify, rollUserVerify } from "../middleware/rollVerify.js"
 
 const viewsController = new ViewsController
-const loginController = new LoginController
+
 
 const router = Router()
-router.get('auth/login', loginController.loginRender)
+
 router.get('/products', auth, viewsController.productsRender)
 
 router.get('/carts/:cid', viewsController.cartsRender)
