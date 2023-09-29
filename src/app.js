d41b581dcde2985217b568333dd5d35500ca0b29
Tavott <guerreroagustavo@hotmail.com>
2023-09-28 21:10:15 -0300

    v2

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ const productRouter = new ProductsRouterClass();
 const messageRouterClass = new MessageRouterClass();
 const loggerRouter = new LoggerRouterClass();
 
-initialibzePassport();
+initializePassport();
 app.use(passport.initialize());
 app.use(cookieParser());
 app.use(cors());
